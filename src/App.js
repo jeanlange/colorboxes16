cc44ca23474b6c62590000e9ae1047eb4497f0ac
Jean Lange <jean.f.lange@gmail.com>
2023-06-12 22:10:44 -0400

    random color on hover

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function Box() {
+  const [color, setColor] = useState("#CCC");
+
+  const rando = () => {
+    return Math.floor(Math.random() * 256);
+  }
+  const handleHover = () => {
+    const randomColor = `rgb(${rando()}, ${rando()}, ${rando()})`
+    setColor(randomColor);
+  }
+
   return (
-    <div className="box">
+    <div
+      onMouseEnter={handleHover}
+      className="box"
+      style={{
+        backgroundColor: color
+      }}
+    >
     </div>
   )
 }
